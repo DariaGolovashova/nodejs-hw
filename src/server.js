bdceae6c7119c84b5c6a2550f57d6ce7f77cae61
Daria Golovashova <darja.golovashova@gmail.com>
2026-05-06 11:07:35 +0200

    change server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
